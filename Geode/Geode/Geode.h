688ff355f90be7821706809ba6cd6dc921dfd14b
Kyle Reynolds <kylereynoldsdev@gmail.com>
2024-02-07 22:47:29 -0700

    Add files via upload

---
+++ Geode/Geode/Geode.h
@@ -0,0 +1,18 @@
+//
+//  Geode.h
+//  Geode
+//
+//  Created by Kyle Reynolds on 2/7/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Geode.
+FOUNDATION_EXPORT double GeodeVersionNumber;
+
+//! Project version string for Geode.
+FOUNDATION_EXPORT const unsigned char GeodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Geode/PublicHeader.h>
+
+
